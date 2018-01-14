3c3a5ba312bf086fee54bc5ca31529131ba7c85e
Evgen-PC\Evgen <benichester>
2018-01-14 20:58:56 +0200

    Move gallery in scope

--- js/angular/gallery/gallery.Controller.js
+++ js/angular/gallery/gallery.Controller.js
@@ -3,5 +3,109 @@
 	.module('app')
 	.controller('galleryController', galleryController);
 	
+	galleryController.inject =['$scope']
 	
+	function galleryController($scope) {
+    $scope.gallery = [{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery1.jpg',
+        thumbnail: '/img/Gallery/small/Gallery1_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery2.jpg',
+        thumbnail: '/img/Gallery/small/Gallery2_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery3.jpg',
+        thumbnail: '/img/Gallery/small/Gallery3_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery4.jpg',
+        thumbnail: '/img/Gallery/small/Gallery4_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery5.jpg',
+        thumbnail: '/img/Gallery/small/Gallery5_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery6.jpg',
+        thumbnail: '/img/Gallery/small/Gallery6_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery7.jpg',
+        thumbnail: '/img/Gallery/small/Gallery7_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery8.jpg',
+        thumbnail: '/img/Gallery/small/Gallery8_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery9.jpg',
+        thumbnail: '/img/Gallery/small/Gallery9_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery10.jpg',
+        thumbnail: '/img/Gallery/small/Gallery10_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery11.jpg',
+        thumbnail: '/img/Gallery/small/Gallery11_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery12.jpg',
+        thumbnail: '/img/Gallery/small/Gallery12_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery13.jpg',
+        thumbnail: '/img/Gallery/small/Gallery13_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery14.jpg',
+        thumbnail: '/img/Gallery/small/Gallery14_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery15.jpg',
+        thumbnail: '/img/Gallery/small/Gallery15_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery16.jpg',
+        thumbnail: '/img/Gallery/small/Gallery16_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery17.jpg',
+        thumbnail: '/img/Gallery/small/Gallery17_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery18.jpg',
+        thumbnail: '/img/Gallery/small/Gallery18_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery19.jpg',
+        thumbnail: '/img/Gallery/small/Gallery19_s.jpg',
+  		},
+		{
+        alt: 'Subaru BRZ',
+        url: '/img/Gallery/Gallery20.jpg',
+        thumbnail: '/img/Gallery/small/Gallery20_s.jpg',
+  		}];
+	};
+
 })();
